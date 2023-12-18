e44db4193bdf898ca40bd025c0a408b084a84251
Deepak Kumar <kumard18791@gmail.com>
2023-12-18 16:44:54 +0530

    Comment Helmet library

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import {BrowserRouter,Routes,Route}from 'react-router-dom'
 import Home from './Pages/Home.jsx';
-import {Helmet}from 'react-helmet'
+// import {Helmet}from 'react-helmet'
 function App() {
   return (
     <div>
-      <Helmet>
+      {/* <Helmet>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&display=swap"
         />
-      </Helmet>
+      </Helmet> */}
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
